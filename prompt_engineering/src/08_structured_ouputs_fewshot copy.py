a50309f23a26516c31aba5a230e56d0fad0df9dc
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2024-09-23 12:24:00 -0500

    completed the pydantic model validation

--- prompt_engineering/src/08_structured_ouputs_fewshot copy.py
+++ prompt_engineering/src/08_structured_ouputs_fewshot copy.py
@@ -1,11 +1,8 @@
-import json
 import os
 
 from dotenv import load_dotenv
-from model.flight_model import Booking
 from openai import OpenAI
 from openai.types.chat.chat_completion import ChatCompletion
-from pydantic import ValidationError
 
 load_dotenv()
 
@@ -63,66 +60,23 @@ def ask_openai(
     return response
 
 
-def extract_flight_info():
-    text = """
-    Emily Thompson booked a flight on October 10, 2024. She will be flying from New York (JFK) to Los Angeles (LAX) on flight number AA123. 
-    The departure time is 8:00 AM, and the arrival time is 11:30 AM. She has a carry-on bag and a checked bag. 
-    Her ticket price was $450.00, and she will be seated in 14A.
-    """
-
-    prompt = f"""
-    Extract the key information from the following text delimited by triple backticks and format it in JSON.
-    I need details like name, booking date, flight information (flight number, origin, destination, departure/arrival times), 
-    luggage details, ticket price, and seat number.
-
-    Text: ```{text}```
-   
-
-    """
-    response = ask_openai(prompt=prompt)
-    # Print the Type and Response
-    return response
-
-
-def process_flight_info(json_data) -> Booking:
-    # Load JSON data
-    data = json.loads(json_data)
-
-    # Parse and validate using the Booking model
-    try:
-        booking = Booking(**data)
-        print("Validation Successful!")
-        print(booking.model_dump_json(indent=4, exclude_unset=True))
-        return booking
-    except ValidationError as e:
-        print("Validation Error:")
-        print(e.json(indent=4))
-
-
 def extract_flight_info_few_shot():
-    # text = """
-    # Emily Thompson booked a flight on October 10, 2024. She will be flying from New York (JFK) to Los Angeles (LAX) on flight number AA123.
-    # The departure time is 8:00 AM, and the arrival time is 11:30 AM. She has a carry-on bag and a checked bag.
-    # Her ticket price was 450.00$, and she will be seated in 14A.
-    # """
     text = """
     Emily Thompson booked a flight on October 10, 2024. She will be flying from New York (JFK) to Los Angeles (LAX) on flight number AA123.
     The departure time is 8:00 AM, and the arrival time is 11:30 AM. She has a carry-on bag and a checked bag.
-    Her ticket price was 450.00€, and she will be seated in 14A.
+    Her ticket price was 450.00$, and she will be seated in 14A.
     """
+    # text = """
+    # Emily Thompson booked a flight on October 10, 2024. She will be flying from New York (JFK) to Los Angeles (LAX) on flight number AA123.
+    # The departure time is 8:00 AM, and the arrival time is 11:30 AM. She has a carry-on bag and a checked bag.
+    # Her ticket price was 450.00€, and she will be seated in 14A.
+    # """
 
-    text = """
-    Li Wei booked a flight on November 5, 2024. He will be flying from Beijing (PEK) to Shanghai (PVG) on flight number CA456. 
-    The departure time is 10:00 AM, and the arrival time is 12:30 PM. He has a carry-on bag and one checked bag. 
-    His ticket price was ¥3,200.00, and he will be seated in 22C.
-    """
-
-    additional_info = """
-    Currencies should be mentioned only in text:
-    Examples are:
-    $ should be represented as DOLLAR
-    € should be represented as EURO
-    """
+    # text = """
+    # Li Wei booked a flight on November 5, 2024. He will be flying from Beijing (PEK) to Shanghai (PVG) on flight number CA456.
+    # The departure time is 10:00 AM, and the arrival time is 12:30 PM. He has a carry-on bag and one checked bag.
+    # His ticket price was ¥3,200.00, and he will be seated in 22C.
+    # """
 
     few_shot_example = ""
     with open("prompt_engineering/resources/flight-info-fewshot.json", "r") as file:
@@ -133,17 +87,13 @@ def extract_flight_info_few_shot():
     I need details like name, booking date, flight information (flight number, origin, destination, departure/arrival times), 
     luggage details, ticket price, and seat number.
 
-    Please do not include json and ``` in the generated JSON.
-
     Here is an example output of the JSON format:\n
     {few_shot_example}
 
     \n
-    {additional_info}
 
     Text: ```{text}```
    
-
     """
     response = ask_openai(prompt=prompt)
     # Print the Type and Response
@@ -151,17 +101,5 @@ def extract_flight_info_few_shot():
 
 
 if __name__ == "__main__":
-    # Travel - Retrieve Flight Info
-    # response: ChatCompletion = extract_flight_info()
-    # print(f"response  : {response.choices[0].message.content}")
-
-    # Travel - Retrieve Flight Info with Few Shot
-    # response: ChatCompletion = extract_flight_info()
-    # print(f"response  : {response.choices[0].message.content}")
     response: ChatCompletion = extract_flight_info_few_shot()
-    json_data = response.choices[0].message.content
-    print(f"json_data  : {json_data}")
-
-    # Call the function with the json_data
-    booking = process_flight_info(json_data)
-    print(f"booking : {booking}")
+    print(f"response  : {response.choices[0].message.content}")
